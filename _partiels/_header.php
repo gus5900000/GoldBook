21977f4f62f1f952a87cfa56d11a124d42ddd477
gus5900000 <augustinverissimo.pro@gmail.com>
2024-07-25 23:48:42 +0200

    Add Header

--- _partiels/_header.php
+++ _partiels/_header.php
@@ -1,4 +1,29 @@
-<?php
+<header class="header">
+    <div class="header-top">
+        <div class="header-logo">
+            <img src="../assets/logo.png" alt="Logo" id="logo">
+        </div>
+        <div class="header-titre">
+            <h1>GoldBook</h1>
+        </div>
+        <div class="header-login">
+            <form action="#" method="POST">
+                <label for="identifiant">Identifiant</label>
+                <input type="text" id="identifiant" name="identifiant" placeholder="Mettez votre identifiant">
 
+                <label for="password">Mot de passe</label>
+                <input type="text" id="password" name="password" placeholder="Mettez votre mot de passe">
 
-?>
+                <input type="submit" value="Se connecter">
+            </form>
+        </div>
+    </div>
+    <div class="header-bottom">
+            <div class="header-bottom-img">
+                <div class="header-bottom-texte">
+                    <h1>Livre d'or</h1>
+                    <p>Ce site permet de publier un message qui sera visible par les autres utilisateurs</p>
+                </div>
+            </div>
+        </div>
+</header>
